7d32584757e4bcfa61607a1896e6a70d1d7915f4
adil <adilaitelhoucine1@gmail.com>
2024-10-17 16:28:38 +0200

    almost all done exept responsive and some details in dark contact mode

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const button = document.querySelector(".addtocart");
-const done = document.querySelector(".done");
-console.log(button);
-let added = false;
-button.addEventListener('click',()=>{
-if(added){
-    done.style.transform = "translate(-110%) skew(-40deg)";
-    added = false;
+function ToDarkMode(){
+    document.body.classList.toggle("dark-mode");
+    var links=document.querySelectorAll("a");
+    var ftr=document.querySelector("footer");
+    var titlecard=document.querySelectorAll("h6");
+    var contacttitle=document.querySelectorAll("h1");
+    var paragraphs=document.querySelectorAll("p");
+    var section2=document.querySelector(".section-2");
+    var section4=document.querySelector(".section-4");
+    ftr.classList.toggle("footer-dark");
+    links.forEach((element) => element.classList.toggle("dark-header"));
+    titlecard.forEach((element) => element.classList.toggle("dark-header")); 
+    paragraphs.forEach((element) => element.classList.toggle("white-text"));
+    section2.classList.toggle("dark-header");
+    section4.classList.toggle("dark-header");
+    //titlecontact.classList.replace("dark-header"); 
 }
-else{
-    done.style.transform = "translate(0px)";
-    added = true;
-    console.log("non-clicked");
-}   
-});
+
+

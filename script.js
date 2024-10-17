930e4ddb93d9ae350884d1c4ef24379a5f6f4e9a
adil <adilaitelhoucine1@gmail.com>
2024-10-17 17:18:35 +0200

    the contact issues fixed and still the responsive side

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function ToDarkMode(){
     var links=document.querySelectorAll("a");
     var ftr=document.querySelector("footer");
     var titlecard=document.querySelectorAll("h6");
-    var contacttitle=document.querySelectorAll("h1");
+    var contacttitle=document.querySelectorAll("sidebar-title");
     var paragraphs=document.querySelectorAll("p");
     var section2=document.querySelector(".section-2");
     var section4=document.querySelector(".section-4");
@@ -13,7 +13,8 @@ function ToDarkMode(){
     paragraphs.forEach((element) => element.classList.toggle("white-text"));
     section2.classList.toggle("dark-header");
     section4.classList.toggle("dark-header");
-    //titlecontact.classList.replace("dark-header"); 
+    contacttitle.classList.replace("white-text"); 
 }
+
 
 

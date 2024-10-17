fc59853f795e5cc7a2f5f756c59c95baccbfe44f
adil <adilaitelhoucine1@gmail.com>
2024-10-18 00:11:36 +0200

    all is done still working in the scroll btn in dark mode and responsivite

--- script.js
+++ script.js
@@ -5,16 +5,40 @@ function ToDarkMode(){
     var titlecard=document.querySelectorAll("h6");
     var contacttitle=document.querySelectorAll("sidebar-title");
     var paragraphs=document.querySelectorAll("p");
+    var darkscroolbtn=document.querySelectorAll("#myBtn");
     var section2=document.querySelector(".section-2");
     var section4=document.querySelector(".section-4");
     ftr.classList.toggle("footer-dark");
+    var niba=document.querySelector(".sidebar");
+
     links.forEach((element) => element.classList.toggle("dark-header"));
     titlecard.forEach((element) => element.classList.toggle("dark-header")); 
     paragraphs.forEach((element) => element.classList.toggle("white-text"));
     section2.classList.toggle("dark-header");
     section4.classList.toggle("dark-header");
     contacttitle.classList.replace("white-text"); 
+    niba.classList.toggle("test");
+}
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+    ToDarkMode();
+})
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
-
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
